d36c0c620f124e3ed6c0cac2d925a5eeb94b1ea5
swapnilgotsurve <swapnilgotsurve5042@gmail.com>
2026-04-01 09:24:13 +0530

    added promise program

---
+++ problem2.js
@@ -0,0 +1,16 @@
+let myPromise = new Promise(function(resolve, reject) {
+    let n = 5
+    if (n > 0) {
+        resolve("Promise resolved successfully!")
+    }
+    else {
+        reject("Promise rejected!")
+    }
+});
+
+
+myPromise.then(function(successMessage) {
+    console.log(successMessage)
+}).catch(function(errorMessage) {
+    console.log(errorMessage)
+})
